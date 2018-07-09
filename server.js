a01914141e80dcbd16ad63792ef38e5550e9d659
carriecarter <carrie@carrie-carter.com>
2018-07-08 18:05:37 -0700

    pg 14

--- server.js
+++ server.js
@@ -1,17 +1,12 @@
 var http = require('http');
 
-http.createServer(function(request, response) {
+function onRequest(request, response) {
+    console.log('request received.');
     response.writeHead(200, { 'Content-Type': 'text/plain' });
     response.write('Hello World');
     response.end();
-}).listen(8888);
-
-function say(word) {
-    console.log(word);
 }
 
-function execute(someFunction, value) {
-    someFunction(value);
-}
+http.createServer(onRequest).listen(8888);
 
-execute(say, 'Hello');
+console.log('Server Has Started.');
