0c2d51c08ded6b11977099daace99d557bd6fd0c
carriecarter <carrie@carrie-carter.com>
2018-07-08 20:08:43 -0700

    image showing

--- requestHandlers.js
+++ requestHandlers.js
@@ -32,8 +32,8 @@ function upload(response, postData) {
 
 function show(response) {
     console.log('request handler "show" was called. ');
-    response.writeHead(200, {'Content-Type': 'image/png' });
-    fs.createReadStream('/tmp/test.png').pipe(response);
+    response.writeHead(200, { 'Content-Type': 'image/png' });
+    fs.createReadStream('./tmp/test.png').pipe(response);
 }
 
 exports.start = start;
--- server.js
+++ server.js
@@ -4,13 +4,13 @@ var url = require('url');
 function start(route, handle) {
     function onRequest(request, response) {
         var postData = '';
-        var pathname = url.parse(request.url).pathname; 
+        var pathname = url.parse(request.url).pathname;
         console.log('Request for' + pathname + 'received. ');
 
         request.setEncoding('utf8');
 
         request.addListener('data', function(postDataChunk) { postData += postDataChunk;
-            console.log('Received POST data chunk \''+ postDataChunk + '\'.');
+            console.log('Received POST data chunk \'' + postDataChunk + '\'.');
         });
 
         request.addListener('end', function() {
